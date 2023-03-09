b1841d0b6dd30436e69ad16f9edbeb778c5cb46f
Orosz-Zzsombor <101347624+Orosz-Zzsombor@users.noreply.github.com>
2023-03-09 16:08:51 +0100

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void btnRogzit_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtNev.Text =="")
+            {
+                MessageBox.Show("Nem adtál meg nevet");
+            }
+            
+           
+        }
+
+        private void sliJegy_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            lblJegy.Content = sliJegy.Value;
+        }
+    }
+}
